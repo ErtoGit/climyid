810e988340e3c049e6e1ef4e7d7459af78aad0d7
Ertomedia <support@ertomedia.com>
2020-04-22 09:29:42 +0800

    updates

--- static/checkupdate.sh
+++ static/checkupdate.sh
@@ -2,16 +2,16 @@
 
 echo "Do you wish to check for updates?"
 select yn in "Yes" "No"; do
-        case $yn in
-                Yes ) sudo apt update -y; apt list --upgradeable; break;;
-                No ) exit;;
-        esac
+    case $yn in
+        Yes ) sudo apt update -y; apt list --upgradeable; break;;
+        No ) exit;;
+    esac
 done
 sleep 1
 echo "Do you wish to upgrade?"
 select yn in "Yes" "No"; do
-        case $yn in
-                Yes ) sudo apt upgrade -y; break;;
-                No ) exit;;
-        esac
+    case $yn in
+        Yes ) sudo apt upgrade -y; break;;
+        No ) exit;;
+    esac
 done
